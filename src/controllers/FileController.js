a0356e9c47045357f01d6c9434268a97864818c3
Felipe <felipekaires@hotmail.com>
2019-04-14 20:33:06 -0300

    ident

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,21 +1,21 @@
-const File = require('../models/File');
-const Box = require('../models/Box');
+const File = require("../models/File");
+const Box = require("../models/Box");
 
-class FileController{
-    async store(req,res) {
-        // Criar arquivo
-        const box = await Box.findById(req.params.id);
-        const file = await File.create({
-            title: req.file.originalname,
-            path: req.file.key
-        })
-        box.files.push(file);
-        await box.save();
+class FileController {
+  async store(req, res) {
+    // Criar arquivo
+    const box = await Box.findById(req.params.id);
+    const file = await File.create({
+      title: req.file.originalname,
+      path: req.file.key
+    });
+    box.files.push(file);
+    await box.save();
 
-        req.io.sockets.in(box._id).emit('file',file);
+    req.io.sockets.in(box._id).emit("file", file);
 
-        return res.json(file);
-    }
+    return res.json(file);
+  }
 }
 
 module.exports = new FileController();
--- src/server.js
+++ src/server.js
@@ -1,34 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-const server = require('http').Server(app);
-const io  = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 // Realtime
 io.on("connection", socket => {
-    socket.on('connectRoom', box => {
-        socket.join(box);
-    })
+  socket.on("connectRoom", box => {
+    socket.join(box);
+  });
 });
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0-tejxt.mongodb.net/test?retryWrites=true',
-{
-    useNewUrlParser : true,
-});
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0-tejxt.mongodb.net/test?retryWrites=true",
+  {
+    useNewUrlParser: true
+  }
+);
 
-app.use( (req,res, next) =>{
-    req.io = io;
-    return next();
-})
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
-app.use(require('./routes'));
+app.use(express.urlencoded({ extended: true }));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
+app.use(require("./routes"));
 
 server.listen(process.env.PORT || 3338);
