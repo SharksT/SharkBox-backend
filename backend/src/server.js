813b4a89ec719959c0a2c9eeb6a365802651d0fd
Felipe <felipekaires@hotmail.com>
2019-04-14 18:21:08 -0300

    initial models

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0-tejxt.mongodb.net/test?retryWrites=true',
+{
+    useNewUrlParser : true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
