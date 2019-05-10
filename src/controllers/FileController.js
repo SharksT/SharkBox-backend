8a8525f8c94258de2e35494a55a397ef70606b3c
Felipe <felipekaires@hotmail.com>
2019-05-10 18:51:56 -0300

    a

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,16 +1,26 @@
 const File = require("../models/File");
 const Box = require("../models/Box");
-
+const User = require("../models/User");
 class FileController {
   async store(req, res) {
     // Criar arquivo
-    const box = await Box.findById(req.params.id);
     const file = await File.create({
       title: req.file.originalname,
       path: req.file.key
     });
-    box.files.push(file);
-    await box.save();
+    try {
+      const user = await User.findById(req.params.id);
+      user.files.push(file);
+      await user.save();
+    } catch {
+      try {
+        const box = await Box.findById(req.params.id);
+        box.files.push(file);
+        await box.save();
+      } catch (err) {
+        return res.status(400).send({ error: "ID not found" });
+      }
+    }
 
     req.io.sockets.in(box._id).emit("file", file);
 
