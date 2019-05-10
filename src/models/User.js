245dce4672f0d9444a4e0bad2b0431cc332d5154
Felipe <felipekaires@hotmail.com>
2019-05-10 19:30:23 -0300

    auth

--- src/models/User.js
+++ src/models/User.js
@@ -24,10 +24,8 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre("save", async function(next) {
-  if (this.boxes === []) {
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash;
-  }
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
 
   next();
 });
