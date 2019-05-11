537d6502501b9d7b8cfed0a2ff7d7647c7234f23
Felipe <felipekaires@hotmail.com>
2019-05-11 10:09:51 -0300

    Auth0.6

--- src/models/User.js
+++ src/models/User.js
@@ -24,8 +24,8 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre("save", async function(next) {
-  const hash = await bcrypt.hash(this.password, 10);
-  this.password = hash;
+  // const hash = await bcrypt.hash(this.password, 10);
+  //this.password = hash;
 
   next();
 });
